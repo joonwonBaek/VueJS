add13da06ee5266b663f1965439550f85ed46c98
Baek <woonn97@gmail.com>
2023-12-01 16:04:28 +0900

    Create Project

---
+++ Vue-Webpack/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"],
+    alias: {
+      // 경로 별칭
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, // test는 정규표현식 사용해야함
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      //favicon 적용
+      patterns: [{ from: "static" }],
+    }),
+  ],
+  devServer: {
+    port: 1234,
+  },
+};
